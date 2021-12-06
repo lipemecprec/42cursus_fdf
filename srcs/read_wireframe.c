85fdea90733f44e07a7e82c59af7b5e45dacf0a8
Felipe Aguilar <lipemecprec@yahoo.com.br>
2021-12-06 11:42:49 -0300

    Bugfix - Trash data on second line of t_wireframe data
    GNL funcion handles call from different files for reading,
    but as in the read_wireframe function the file descriptor, after close and open,
    were repeated, gnl funcion buffer was not empty, and trash data was input into t_wireframe data.

--- srcs/read_wireframe.c
+++ srcs/read_wireframe.c
@@ -6,7 +6,7 @@
 /*   By: faguilar <faguilar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 23:17:43 by faguilar          #+#    #+#             */
-/*   Updated: 2021/12/04 21:27:36 by faguilar         ###   ########.fr       */
+/*   Updated: 2021/12/06 11:38:38 by faguilar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,16 +48,12 @@ static int	count_row(char *file_name)
 
 	fd = open(file_name, O_RDONLY);
 	row = 0;
-	while (1)
+	line = get_next_line(fd);
+	while (line)
 	{
-		line = get_next_line(fd);
-		if (line == NULL)
-		{
-			free(line);
-			break ;
-		}
-		row++;
 		free(line);
+		line = get_next_line(fd);
+		row++;
 	}
 	close(fd);
 	return (row);
@@ -69,9 +65,15 @@ static int	count_col(char *file_name)
 	int		fd;
 	char	*line;
 
-	fd = open(file_name, O_RDONLY);
+	fd = open(file_name, O_RDONLY, 1);
 	line = get_next_line(fd);
+	line = ft_strtrim(line, " ");
 	col = ft_wordcount(line, ' ');
+	while (line)
+	{
+		free(line);
+		line = get_next_line(fd);
+	}
 	free(line);
 	close(fd);
 	return (col);
@@ -82,11 +84,13 @@ static void	write_data(int *z_data, char *line)
 	char	**nums;
 	int		i;
 
+	line = ft_strtrim(line, " \n");
 	nums = ft_split(line, ' ');
 	i = 0;
 	while (nums[i])
 	{
 		z_data[i] = ft_atoi(nums[i]);
+		nums[i] = NULL;
 		free(nums[i]);
 		i++;
 	}
@@ -100,24 +104,23 @@ void	read_wireframe(t_wireframe *data, char *file_name)
 	char	*line;
 
 	data->height = count_row(file_name);
+	printf("ROWS: %d\t", data->height);
 	data->width = count_col(file_name);
+	printf("ROWS: %d\n", data->width);
 	data->z_grid = (int **)malloc(sizeof(int *) * (data->height + 1));
 	i = 0;
 	while (i <= data->height)
 		data->z_grid[i++] = (int *)malloc(sizeof(int) * (data->width + 1));
 	fd = open(file_name, O_RDONLY);
-	i = 0;
-	while (1)
+	i = -1;
+	line = get_next_line(fd);
+	while (line)
 	{
-		line = get_next_line(fd);
-		if (line == NULL)
-		{
-			free(line);
-			break ;
-		}
-		write_data(data->z_grid[i++], line);
+		write_data(data->z_grid[++i], line);
 		free(line);
+		line = get_next_line(fd);
 	}
+	free(line);
 	data->z_grid[data->height] = NULL;
 	close (fd);
 }
