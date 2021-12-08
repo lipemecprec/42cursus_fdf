4ec6f03792533ebbd73cda7b3049400c48c6c794
Felipe Aguilar <lipemecprec@yahoo.com.br>
2021-12-08 18:03:37 -0300

    refactoring - shutdown

--- srcs/shutdown.c
+++ srcs/shutdown.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   free_memory.c                                      :+:      :+:    :+:   */
+/*   shutdown.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: faguilar <faguilar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 21:27:22 by faguilar          #+#    #+#             */
-/*   Updated: 2021/12/04 21:29:27 by faguilar         ###   ########.fr       */
+/*   Updated: 2021/12/07 16:58:04 by faguilar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libfdf.h"
 
-void	free_wireframe_data(t_wireframe *data)
+static void	free_wireframe_data(t_wireframe *data)
 {
 	int	i;
 
@@ -20,4 +20,14 @@ void	free_wireframe_data(t_wireframe *data)
 	while (i <= data->height)
 		free(data->z_grid[i++]);
 	free(data->z_grid);
+}
+
+void	shutdown(t_wireframe *data)
+{
+	// mlx_clear_window(data->mlx_ptr, data->win_ptr);
+	// mlx_loop_end(data->mlx_ptr);
+	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	// mlx_destroy_image(data->mlx_ptr, data->win_ptr);
+	// mlx_destroy_display(data->mlx_ptr);
+	// free_wireframe_data(data);
 }
