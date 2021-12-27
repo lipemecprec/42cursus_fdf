c058de3e3638627637f716669517a91b0581619e
lipemecprec <lipemecprec@yahoo.com.br>
2021-12-27 15:37:49 -0300

    Memory alloc utils

--- Makefile
+++ Makefile
@@ -17,6 +17,7 @@ SRCS = main.c \
 		$(SRCS_PATH)/read_wireframe.c \
 		$(SRCS_PATH)/shutdown.c \
 		$(SRCS_PATH)/strline.c \
+		$(SRCS_PATH)/utils.c \
 
 all: $(NAME)
 
---
+++ srcs/utils.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faguilar <faguilar@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/26 18:07:33 by faguilar          #+#    #+#             */
+/*   Updated: 2021/12/26 18:11:29 by faguilar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libfdf.h"
+
+void	*mgrant(int size)
+{
+	void	*memory_alloc;
+
+	memory_alloc = malloc(size);
+	if (!memory_alloc)
+		farewell(NULL, 2);
+	else
+		return (memory_alloc);
+}
+
+void	set_free(void *ptr)
+{
+	free(ptr);
+	ptr = NULL;
+}
